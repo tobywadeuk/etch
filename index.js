7ccf2bdb4e82d0ed65dfad0fd9c71d85a3e868e0
Toby <tobyxs91@gmail.com>
2022-03-19 13:12:59 +0100

    m

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 // creating container for the divs and blank array for later
 let containerT = document.querySelector(".containerDiv")
 let divArray = []
+let colourArray = []
+let colourPrompt = "single"
 
 // build square and introduce hover functionality
 buildSquare(16)
 divHover()
+
+let colorButton = document.querySelector(".colorbutton")
+colorButton.addEventListener("click", function() {
+  colorButton.classList.toggle("colorbuttonclick")
+  if (colourPrompt == "single") {
+    colourPrompt = "rainbow"
+  } else if (colourPrompt == "rainbow") {
+    colourPrompt = "single"
+  }
+})
 
 // function make the divs change colour when u enter them with your mouseenter
 // select all the divs and put them in an array
 // when these divs are entered, make em blue
 function divHover(userReq) {
   let divArray = Array.from(document.querySelectorAll(".bigDivs"))
-  for (let i = 0; i < (divArray.length); i++) {
-    divArray[i].addEventListener("mouseenter", function() {
-       divArray[i].style.backgroundColor = "#46244C"
-    })
-  }
+  // let colourPrompt = prompt("What colours would you like: single or rainbow?")
+  if (colourPrompt == "single") {
+    for (let i = 0; i < (divArray.length); i++) {
+      divArray[i].addEventListener("mouseenter", function() {
+        divArray[i].style.backgroundColor = "#46244C"
+      })
+    }
+  } else if (colourPrompt == "rainbow")
+    for (let i = 0; i < (divArray.length); i++) {
+      colourArray[i] = ("#" + ((1 << 24) * Math.random() | 0).toString(16))
+      divArray[i].addEventListener("mouseenter", function() {
+        divArray[i].style.backgroundColor = colourArray[i];
+      })
+    }
 }
 
 // build squares with loop based on input
@@ -31,13 +52,15 @@ function buildSquare(userReq) {
     columnsStyle = ((window.screen.width - 20) / userReq) + "px"
     let columnsStyleComb = ''
     for (let i = 0; i < userReq; i++) {
-      columnsStyleComb = columnsStyleComb + columnsStyle.toString() + " "}
+      columnsStyleComb = columnsStyleComb + columnsStyle.toString() + " "
+    }
     containerT.style.gridTemplateColumns = columnsStyleComb;
 
     rowsStyle = (window.screen.length - 20 / userReq) + "px"
     let rowsStyleComb = ''
     for (let i = 0; i < userReq; i++) {
-      rowsStyleComb = rowsStyleComb + rowsStyle.toString() + " "    }
+      rowsStyleComb = rowsStyleComb + rowsStyle.toString() + " "
+    }
     containerT.style.gridTemplateRows = rowsStyleComb;
   }
 }
@@ -47,7 +70,7 @@ function buildSquare(userReq) {
 // careful if they take the piss
 //otherwise remove divs, build new square and introduce divhover
 
-document.querySelector("button").addEventListener("click", function() {
+document.querySelector(".mainbtn").addEventListener("click", function() {
   let divArray = Array.from(document.querySelectorAll(".bigDivs"))
   let userReq = prompt("How many squares this time?")
   if (userReq > 0 && userReq < 65) {
